cc9cff0e8b4ac293ae4e62825d1c8affba069c30
Rohit <rohitbelure128@gmail.com>
2021-10-14 21:13:46 +0530

    Custom Error Page

--- hello.py
+++ hello.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/index')
-def index():
-	return render_template('index.html')
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
 
-@app.route('/user/<name>')
-def user(name):
-	return render_template('user.html', name=name)
+@app.errorhandler(500)
+def internal_server_error(e):
+	return render_template('500.html'), 500
 
 if __name__ == '__main__':
 	app.run(debug=True)
