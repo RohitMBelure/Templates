7f07a95aa207dec6b665fc55b61e37f9bde7946c
Rohit Belure <90506564+RohitMBelure@users.noreply.github.com>
2021-10-18 16:25:27 +0530

    Usage of user.html file

--- hello.py
+++ hello.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html', name=name)
+
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template('404.html'), 404
